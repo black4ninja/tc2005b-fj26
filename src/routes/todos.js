dd225cbf34bdea610660861bd4e89eac8977523e
Alejandro Fernández <alfervil4@gmail.com>
2026-04-12 17:07:16 -0600

    feat(sort-alphabetical): orden alfabético con ?sort=title

--- src/routes/todos.js
+++ src/routes/todos.js
@@ -4,7 +4,16 @@ const store = require('../store');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  const todos = store.listTodos();
+  const all = store.listTodos();
+  const { sort } = req.query;
+  let todos = all;
+  if (sort === 'title') {
+    todos = all.sort((a, b) => {
+      if (a.title > b.title) return 1;
+      if (a.title < b.title) return -1;
+      return 0;
+    });
+  }
   res.render('todos/index', { todos });
 });
 
